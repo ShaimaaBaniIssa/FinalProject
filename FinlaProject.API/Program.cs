315ed5d413371160446b440dfbb349de17c8fc47
Rawantaani <rawantaani2001@gmail.com>
2024-10-01 14:32:42 +0300

    create common&Services&Repository folders create DbContext& IDbContext and add ConnectionStrings to json file .

--- FinlaProject.API/Program.cs
+++ FinlaProject.API/Program.cs
@@ -1,3 +1,6 @@
+using FinalProject.Core.Common;
+using FinalProject.Infra.Common;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<IDbContext, DbContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
