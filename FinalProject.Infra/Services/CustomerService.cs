3e9df134e236d6735c38d3adbaef15fdacfdd6ff
Mohammadalomari01 <mohammad5112001@gmail.com>
2024-10-02 18:21:26 +0300

    test

--- FinalProject.Infra/Services/CustomerService.cs
+++ FinalProject.Infra/Services/CustomerService.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace FinalProject.Infra.Services
 {
     public class CustomerService : ICustomerService
-    {
+    {//hello
         private readonly ICustomerRepository _customerRepository;
         public CustomerService (ICustomerRepository customerRepository)
         {
