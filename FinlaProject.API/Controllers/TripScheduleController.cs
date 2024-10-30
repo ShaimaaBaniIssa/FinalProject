3b911b390fb8065c3575f9cfc5d07ce0efa28873
Shaimaa Ali <shaymaali121@gmail.com>
2024-10-30 21:50:34 +0300

    Validate hour format

--- FinlaProject.API/Controllers/TripScheduleController.cs
+++ FinlaProject.API/Controllers/TripScheduleController.cs
@@ -34,6 +34,24 @@ namespace FinalProject.API.Controllers
         [Route("CreateTripSchedule")]
         public IActionResult CreateTripSchedule(Tripschedule tripSchedule)
         {
+
+            // validate the hour
+            bool isValidDeparturetime = DateTime.TryParseExact(tripSchedule.Departuretime, "HH:mm",
+                null, System.Globalization.DateTimeStyles.None,
+                out DateTime departuretime);
+            bool isValidArrivaltime = DateTime.TryParseExact(tripSchedule.Arrivaltime, "HH:mm",
+                null, System.Globalization.DateTimeStyles.None,
+               out DateTime arrivaltime);
+
+            if (!isValidDeparturetime || !isValidArrivaltime)
+            {
+                return BadRequest("Hour must be in HH:mm format");
+            }
+
+
+            if (arrivaltime.CompareTo(departuretime) != 1)
+                return BadRequest("Arrival time must be greater than Departure time"); ;
+
             // check first if the train is avaialble at the selected date
             bool isAvailable = _tripScheduleService.CheckTrainAvailabilty((int)tripSchedule.Trainid, tripSchedule.Tdate.Value, tripSchedule.Departuretime);
             if (!isAvailable)
